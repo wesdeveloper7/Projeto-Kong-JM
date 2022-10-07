867c9a957bcdf38fab574339d9bb2e35d9d788a1
wesdeveloper7 <wesdeveloper7@gmail.com>
2022-10-06 21:31:45 -0300

    commit

---
+++ jav/script.js
@@ -0,0 +1,9 @@
+const bntMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    
+const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+bntMobile.addEventListener('click', toggleMenu);
